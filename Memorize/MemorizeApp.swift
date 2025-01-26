b79fdf39868e36d7f6a70d9fad7f0eafccfc6143
Berke Yılmaz <berke.yilmaz@istun.edu.tr>
2025-01-26 13:21:58 +0300

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Berke Yılmaz on 26.01.2025.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
