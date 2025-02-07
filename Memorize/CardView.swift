3a8e656a6343c69e52fe4089999aab94e627513c
Berke Yılmaz <berke.yilmaz@istun.edu.tr>
2025-02-07 22:20:06 +0300

    L9 Update

--- Memorize/CardView.swift
+++ Memorize/CardView.swift
@@ -19,22 +19,28 @@ struct CardView: View {
     
     var body: some View {
         TimelineView (.animation) { timeline in
-            Pie(endAngle: .degrees(card.bonusPercentRemaining * 360))
-                .opacity(Constants.pie.opacity)
-                .overlay(
-                    Text(card.content)
-                        .font(.system(size: Constants.FontSize.largest))
-                        .minimumScaleFactor(Constants.FontSize.scaleFactor)
-                        .multilineTextAlignment(.center)
-                        .aspectRatio(1, contentMode: .fit)
-                        .padding(Constants.pie.inset)
-                        .rotationEffect(.degrees(card.isMatched ? 360 : 0))
-                        .animation(.spin(duaration: 1), value: card.isMatched) //
-                )
-                .padding(Constants.inset)
-                .cardify(isFaceUp: card.isFaceUp)
-                .opacity(card.isFaceUp || !card.isMatched ? 1 : 0)
+            if card.isFaceUp || !card.isMatched {
+                Pie(endAngle: .degrees(card.bonusPercentRemaining * 360))
+                    .opacity(Constants.Pie.opacity)
+                    .overlay(cardContents.padding(Constants.Pie.inset))
+                    .padding(Constants.inset)
+                    .cardify(isFaceUp: card.isFaceUp)
+                    .transition(.scale)
+                
+            } else {
+                Color.clear
+            }
         }
+    }
+    
+    var cardContents: some View {
+        Text(card.content)
+            .font(.system(size: Constants.FontSize.largest))
+            .minimumScaleFactor(Constants.FontSize.scaleFactor)
+            .multilineTextAlignment(.center)
+            .aspectRatio(1, contentMode: .fit)
+            .rotationEffect(.degrees(card.isMatched ? 360 : 0))
+            .animation(.spin(duaration: 1), value: card.isMatched)
     }
     
     private struct Constants {
@@ -46,7 +52,7 @@ struct CardView: View {
             static let smallest: CGFloat = 10
             static let scaleFactor = smallest / largest
         }
-        struct pie {
+        struct Pie {
             static let opacity: CGFloat = 0.4
             static let inset: CGFloat = 15
         }
