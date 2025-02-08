aac9d9c250bd004e2396a2ede92e6b4dffbd0f50
Berke Yılmaz <berke.yilmaz@istun.edu.tr>
2025-02-08 12:37:06 +0300

    L9 Finished

--- Memorize/EmojiMemoryGameView.swift
+++ Memorize/EmojiMemoryGameView.swift
@@ -12,14 +12,21 @@ struct EmojiMemoryGameView: View {
     typealias Card = MemoryGame<String>.Card
     
     @ObservedObject var viewModel: EmojiMemoryGame
-    private let aspectRatio: CGFloat = 2/3
-
+    
+    private struct Constants {
+        static let aspectRatio: CGFloat = 2/3
+        static let dealAnimation: Animation = .easeInOut(duration: 1)
+        static let delayInterval: TimeInterval = 0.08
+        static let deckWidth: CGFloat = 50
+    }
+    
     var body: some View {
         VStack {
-            cards
-                .foregroundColor(.orange)
+            cards.foregroundColor(.orange)
             HStack {
                 score
+                Spacer()
+                deck.foregroundStyle(.orange)
                 Spacer()
                 shuffle
             }
@@ -42,14 +49,54 @@ struct EmojiMemoryGameView: View {
     }
     
     private var cards: some View {
-        AspectVGrid(viewModel.cards, aspectRatio: aspectRatio) { card in
-            CardView(card)
-                .padding(4)
-                .overlay(FlyingNumber(number: scoreChange(causedBy: card)))
-                .zIndex(scoreChange(causedBy: card) != 0 ? 100 : 0)
-                .onTapGesture {
-                    choose(card)
-                }
+        AspectVGrid(viewModel.cards, aspectRatio: Constants.aspectRatio) { card in
+            if isDealt(card) {
+                CardView(card)
+                    .matchedGeometryEffect(id: card.id, in: dealingNameSpace)
+                    .transition(.asymmetric(insertion: .identity, removal: .identity))
+                    .padding(4)
+                    .overlay(FlyingNumber(number: scoreChange(causedBy: card)))
+                    .zIndex(scoreChange(causedBy: card) != 0 ? 100 : 0)
+                    .onTapGesture {
+                        choose(card)
+                    }
+            }
+        }
+    }
+    
+    @State private var dealt = Set<Card.ID>()
+    
+    private func isDealt(_ card: Card) -> Bool {
+        dealt.contains(card.id)
+    }
+    
+    private var undealtCards: [Card] {
+        viewModel.cards.filter { !isDealt($0) }
+    }
+    
+    @Namespace private var dealingNameSpace
+    
+    private var deck: some View {
+        ZStack {
+            ForEach(undealtCards) { card in
+                CardView(card)
+                    .matchedGeometryEffect(id: card.id, in: dealingNameSpace)
+                    .transition(.asymmetric(insertion: .identity, removal: .identity))
+            }
+        }
+        .frame(width: Constants.deckWidth, height: Constants.deckWidth / Constants.aspectRatio)
+        .onTapGesture {
+            deal()
+        }
+    }
+    
+    private func deal() {
+        var delay: TimeInterval = 0
+        for card in viewModel.cards {
+            withAnimation(Constants.dealAnimation.delay(delay)) {
+                _ = dealt.insert(card.id)
+            }
+            delay += Constants.delayInterval
         }
     }
     
